421319235528dfeb56b519f1e3b0023445283c6b
Asuka <cubictown.500c@gmail.com>
2015-06-01 22:19:31 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FurstApp::Application.config.secret_key_base = '1472301fe5f05d6cacdaef98c50a7f0c0d671f76cfb3eca824a0a70c561e5768e29247d7a1b13ca65489da11ec0e601f9b0924e06d62ba4fe91d6693d4dd0270'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FurstApp::Application.config.session_store :cookie_store, key: '_furst_app_session'
